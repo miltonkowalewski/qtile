34184d4a0627d84c25ab6b5c8a76adbe4bdde8ee
Milton Kowalewski <milton.kowalewski@pgmais.com.br>
2022-11-18 18:07:52 -0300

    update

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-nm-applet --indicator &
+type -p nm-applet >/dev/null || nm-applet --indicator &
+type blueman-applet >dev/null || blueman-applet &
 mkdir -p ~/wallpapers &
 type -p feh >/dev/null || sudo apt install feh -y && cp -R /usr/share/backgrounds/* ~/wallpapers &
 feh --randomize --bg-fill ~/wallpapers/* &
--- config.py
+++ config.py
@@ -82,7 +82,29 @@ keys = [
 ]
 
 # groups = [Group(i) for i in "123456789"]
-groups = [Group(i) for i in "123456789"]
+# groups = [Group(name=k, label=label) for k, label in [
+#     ("2", " 2|Web"),
+#     ("1", " 1|Code"),
+#     ("3", "3"),
+#     ("4", "4"),
+#     ("5", "5"),
+#     ("6", "6"),
+#     ("7", " 7|Slack"),
+#     ("8", " 8|Disc"),
+#     ("9", " 9|Term")
+# ]]
+groups = [
+    Group(name="2", label=" 2|Web"),
+    Group(name="1", label=" 1|Code"),
+    Group(name="3", label="3"),
+    Group(name="4", label="4"),
+    Group(name="5", label="5"),
+    Group(name="6", label="6"),
+    Group(name="7", label=" 7|Slack"),
+    Group(name="8", label=" 8|Disc"),
+    Group(name="9", label=" 9|Term")
+]
+# "","","","","","","","",""
 
 for i in groups:
     keys.extend(
@@ -95,16 +117,16 @@ for i in groups:
                 desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
+            # Key(
+            #     [mod, "shift"],
+            #     i.name,
+            #     lazy.window.togroup(i.name, switch_group=True),
+            #     desc="Switch to & move focused window to group {}".format(i.name),
+            # ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name)),
         ]
     )
 
